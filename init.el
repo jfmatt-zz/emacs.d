b670f81d2cf9965a6c26579d6ce1a654735720e0
James Matthews <jfmatt8067@gmail.com>
2015-01-17 18:14:26 -0800

    Update init.el

--- init.el
+++ init.el
@@ -1,112 +1,100 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("elpa" . "http://tromey.com/elpa/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
-			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("elpa" . "http://tromey.com/elpa/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 
-(add-to-list 'load-path "~/.emacs.d/plugins/")
-(add-to-list 'load-path "~/.emacs.d/slime/")
-(require 'slime-autoloads)
-
-; ASM
-(autoload 'nasm-mode "~/.emacs.d/plugins/nasm-mode.el" "" t)
-(add-to-list 'auto-mode-alist '("\\.\\(asm\\|s\\)$" . nasm-mode))
-(setq nasm-basic-offset 2)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; General
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Indenting
 (setq c-default-style "k&r")
 (setq default-tab-width 2)
 (setq tab-width 2)
+(setq-default indent-tabs-mode nil)
 
-; VIM-style % to show matching paren/brace/bracket
+; Keybindings
+;   VIM-style % to show matching paren/brace/bracket
 (global-set-key "%"
-	(lambda
-		(arg)
-		(interactive "p")
-		(cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
-					((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-					(t (self-insert-command (or arg 1))))))
+        (lambda
+                (arg)
+                (interactive "p")
+                (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+                                        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+                                        (t (self-insert-command (or arg 1))))))
+;   Make find-and-replace work on the whole buffer
+(global-set-key (kbd "M-%")
+  (lambda
+    ()
+    (interactive)
+    (save-excursion
+      (beginning-of-buffer)
+      (call-interactively 'replace-string))))
+(global-set-key (kbd "C-?") 'comment-or-uncomment-region)
+;   Window movement
+(windmove-default-keybindings)
+(global-set-key (kbd "C-x C-o") 'find-file-other-window)
 
-; Compilation
-(setq compilation-ask-about-save nil)
-(defun make-make (cmd)
-	(interactive
-	 (list (read-shell-command "Compile command: " "make -k")))
-	(compile cmd))
-(defun make-run ()
-	(interactive)
-	(compile "make run"))
-(defun make-clean ()
-	(interactive)
-	(compile "make clean"))
-(defun bind-make ()
-	"Bind make, recompile, run, and clean to f4-f7"
-	(local-set-key [f4] 'make-make)
-	(local-set-key [f5] 'recompile)
-	(local-set-key [f6] 'make-run)
-	(local-set-key [f7] 'make-clean))
-(add-hook 'c-mode-common-hook 'bind-make)
-								
 ; Interface
 (global-linum-mode t)
 (blink-cursor-mode 0)
 (global-hl-line-mode t)
 (setq-default cursor-type 'box)
+(setq compilation-scroll-output t)
+
+; Minor keybind behavior tweaks
 (global-set-key (kbd "RET") 'newline-and-indent)
+(setq compilation-ask-about-save nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Everything below this line requires that packages be loaded
+; Package configurations
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (package-initialize)
-(defvar my-packages '(
-	; Interface
-	dired-details
-	dedicated
+(dolist
+  (pkg '(
+    ; Interface
+    column-marker
+    dired-details
+    dedicated
+    sr-speedbar
+    workgroups2
 
-	;Autocomplete										
-	auto-complete
-	ac-js2
-	ac-nrepl
+    ; Autocomplete
+    company-ycmd
 
-	; Indentation
-	smart-tabs-mode
-	sws-mode
+    ; Indentation
+    smart-tabs-mode
+    sws-mode
 
-	; Colors
-	color-theme
-	monokai-theme
-	rainbow-delimiters
+    ; Colors
+    color-theme
+    monokai-theme
+    rainbow-delimiters
 
-	; Language modes
-	haskell-mode
-	markdown-mode
-	
-	; Clojure
-	clojure-mode
-	clojure-test-mode
-	nrepl
-	paredit
+    ; Language modes
+    haskell-mode
+    markdown-mode
 
-  ; Web
-	emmet-mode
-	jade-mode
-	js2-mode
-	skewer-mode
-	
-))
-
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
+    ; Web
+    emmet-mode
+    jade-mode
+    js2-mode
+ ))
+  (when (not (package-installed-p pkg))
   (package-refresh-contents)
-    (package-install p)))
+    (package-install pkg)))
 
-; Dired customization
+; Dired config
 (require 'dired-details)
 (setq dired-details-hidden-string "")
 (dired-details-install)
 
 (require 'dired-x)
 (setq-default dired-omit-files-p t)
-(setq dired-listing-switches "-aBhl  --group-directories-first")
+(setq dired-listing-switches "-Bhl --group-directories-first")
+
+(require 'sr-speedbar)
+(global-set-key (kbd "C-c ]") 'sr-speedbar-open)
 
 ; Color theme
 (require 'color-theme)
@@ -114,22 +102,20 @@
 (load-theme 'monokai t)
 
 ; Smart-tab languages
-(smart-tabs-insinuate 'c 'javascript 'java)
+(smart-tabs-insinuate 'c 'javascript 'java 'c++)
 (add-hook 'html-mode-hook 'smart-tabs-mode)
 (smart-tabs-advice js2-indent-line js2-basic-offset)
 
-; autocomplete
-(require 'auto-complete)
-(add-to-list 'ac-modes 'js2-mode)
-(add-to-list 'ac-modes 'js-mode)
+; YCM
+;; Time from the last edit until emacs-ycm starts reparsing the buffer.
+(set-variable 'ycmd-idle-change-delay 2)
+(set-variable 'ycmd-parse-conditions '(save new-line mode-enabled))
 
-(require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(eval-after-load 'js2-mode
-  '(progn
-    (setq js2-auto-indent-p t)
-    (setq js2-enter-indents-newline t)
-    (setq js2-indent-on-enter-key t)))
+; Company Mode
+(require 'company-ycmd)
+(company-ycmd-setup)
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'prog-mode-hook 'ycmd-mode)
 
 (add-hook 'sgml-mode-hook 'emmet-mode)
 (add-hook 'html-mode-hook 'emmet-mode)
@@ -137,42 +123,24 @@
 
 ;; rainbow delimiters
 (require 'rainbow-delimiters)
-(global-rainbow-delimiters-mode)
+(global-rainbow-delimiters-mode t)
 
-;; paredit
-(add-hook 'clojure-mode-hook 'paredit-mode)
-(add-hook 'nrepl-mode-hook 'paredit-mode)
-
-;; nrepl
-(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
-(setq nrepl-popup-stacktraces nil)
-(add-to-list 'same-window-buffer-names "*nrepl*")
-(add-hook 'nrepl-mode-hook 'paredit-mode)
-
-;; ac-nrepl
-(require 'ac-nrepl)
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'nrepl-mode))
-
-;(defun movement-keys ()
-;	(global-set-key [M-left] 'windmove-left)
-;	(global-set-key [M-right] 'windmove-right)
-;	(global-set-key [M-up] 'windmove-up)
-;	(global-set-key [M-down] 'windmove-down))
-;(add-hook 'paredit-mode-hook 'movement-keys)
-(windmove-default-keybindings)
-	
+;; workgroups
+(require 'workgroups2)
+(workgroups-mode 1)
+(setq wg-emacs-exit-save-behavior nil)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(initial-buffer-choice "~/Projects")
+ '(custom-safe-themes (quote ("60f04e478dedc16397353fb9f33f0d895ea3dab4f581307fbf0aa2f07e658a40" default)))
+ '(initial-buffer-choice "~")
  '(js2-mode-indent-ignore-first-tab t)
  '(js2-strict-missing-semi-warning nil)
- '(markdown-command "multimarkdown"))
+ '(markdown-command "multimarkdown")
+ '(speedbar-show-unknown-files t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
